08be1cf5c11b7199cade4170099fd27ff1cadebc
theultraduffmaster <g00318025@gmit.ie>
2016-03-06 17:00:13 +0000

    Commit 3
    Navigate to second page

--- App1/App1/Runner.xaml.cs
+++ App1/App1/Runner.xaml.cs
@@ -13,23 +13,23 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace App1
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Runner : Page
     {
-        public MainPage()
+        public Runner()
         {
             this.InitializeComponent();
         }
 
-        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        private void btnBack_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(Runner));
+            Frame.Navigate(typeof(MainPage));
         }
     }
 }
